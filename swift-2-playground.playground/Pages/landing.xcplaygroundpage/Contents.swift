9906c28a22ed5d1a62c4b454547fe771aa53f6d8
stejmurphy <stephen.jd.murphy@gmail.com>
2016-03-20 14:07:05 +0000

    swift2 set up

---
+++ swift-2-playground.playground/Pages/landing.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var hello = "Hello"; var playground = "playground"
+
+//: 0 to 64
+
+for i in 0..<64{
+    
+    var point = sin(Double(i) * 100 )
+}
+
+//: [Next](@next)
